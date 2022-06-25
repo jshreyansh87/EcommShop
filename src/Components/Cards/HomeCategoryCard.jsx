6ca08c75c57fa30e215930b647588ba0cc733a76
Shreyansh Jain <jshreyansh87@gmail.com>
2022-06-25 13:28:16 +0530

    Updated Category cards

--- src/Components/Cards/HomeCategoryCard.jsx
+++ src/Components/Cards/HomeCategoryCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-const HomeItemCard = ({title, body, imgSrc}) => {
+const HomeCategoryCard = ({title, body, imgSrc}) => {
     return (
         <div className="CategoryCard">
             <div className="CategoryCardImg"><img className='CategoryCardImgSrc' src={`${imgSrc}`}  ></img></div>
@@ -13,4 +13,4 @@ const HomeItemCard = ({title, body, imgSrc}) => {
     )
 }
 
-export default HomeItemCard
+export default HomeCategoryCard
