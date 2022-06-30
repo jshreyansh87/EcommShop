3e7f55dea7fc44e0bbec9ea891ec1b6fb906669b
Shreyansh Jain <jshreyansh87@gmail.com>
2022-06-30 12:51:49 +0530

    Added Skeleton for multiple components

---
+++ src/Components/ProductList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductList = () => {
+  return (
+    <div style={{margin: '10% 0', height: '400px'}}>
+      Display List of Products here
+    </div>
+  )
+}
+
+export default ProductList
