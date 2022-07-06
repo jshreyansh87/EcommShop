da8bd86a0572d18152ac4ff87603cc324a811edc
Shreyansh Jain <jshreyansh87@gmail.com>
2022-07-06 23:52:22 +0530

    Added Product List

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import Navbar from './Components/Utils/Navbar';
 import Home from './Components/Home';
 import Cart from './Components/Cart';
 import Footer from './Components/Utils/Footer';
+import ProductView from './Components/ProductView';
+import ProductList from './Components/ProductList';
 
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/navigation";
 import "swiper/css/thumbs";
-import ProductView from './Components/ProductView';
 
 function App() {
   return (
@@ -24,7 +25,7 @@ function App() {
         
         {/* Update all the routes here */}
 
-        <Route path='about' element={<ProductView />} />
+        <Route path='about' element={<ProductList />} />
         <Route path='wishlist' element={<Home name="Display Wishlist" />} />
         <Route path='cart' element={<Cart /> } />
         <Route path='profile' element={<Home name="Display Profile" />} />
--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -2,8 +2,53 @@ import React from 'react'
 
 const ProductList = () => {
   return (
-    <div style={{margin: '10% 0', height: '400px'}}>
-      Display List of Products here
+    <div className="ProductListBody">
+      <div className='ProductListContainer'>
+
+        <div className="ProductListCard">
+          <div className="ProductListBox">
+              <div className="ProductCoverImage">
+                <img src="https://trek.scene7.com/is/image/TrekBicycleProducts/Cycling_Shoes_Marquee_16x9?$responsive-pjpg$&cache=on,on&wid=1920" />
+              </div>
+            <div className="ProductListContent">
+              <h2>hotstyle</h2>
+              <h3>Running Shoes For Men (Black)</h3>
+              <p>₹247</p>
+              <a href='#'>Shop Now</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="ProductListCard">
+          <div className="ProductListBox">
+              <div className="ProductCoverImage">
+                <img src="https://trek.scene7.com/is/image/TrekBicycleProducts/Cycling_Shoes_Marquee_16x9?$responsive-pjpg$&cache=on,on&wid=1920" />
+              </div>
+            <div className="ProductListContent">
+              <h2>hotstyle</h2>
+              <h3>Running Shoes For Men (Black)</h3>
+              <p>₹247</p>
+              <a href='#'>Shop Now</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="ProductListCard">
+          <div className="ProductListBox">
+              <div className="ProductCoverImage">
+                <img src="https://trek.scene7.com/is/image/TrekBicycleProducts/Cycling_Shoes_Marquee_16x9?$responsive-pjpg$&cache=on,on&wid=1920" />
+              </div>
+            <div className="ProductListContent">
+              <h2>hotstyle</h2>
+              <h3>Running Shoes For Men (Black)</h3>
+              <p>₹247</p>
+              <a href='#'>Shop Now</a>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
     </div>
   )
 }
